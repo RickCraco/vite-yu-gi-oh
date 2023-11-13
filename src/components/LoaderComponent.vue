6afa271981f087d7c4c827fe8e2a841b73be145e
RickCraco <riccardocracolici43@gmail.com>
2023-11-13 16:35:42 +0100

    aggiunto componente loader

---
+++ src/components/LoaderComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>Aspetta le tue carte!</h1>
+</template>
+
+<script>
+    export default {
+        name: 'LoaderComponent',
+    }
+</script>
+
+<style lang="scss" scoped>
+img {
+    width: 100vw;
+}
+</style>
