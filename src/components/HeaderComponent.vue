ac7dcbd634f1a79607821e4118d45fc58f13eb36
RickCraco <riccardocracolici43@gmail.com>
2023-11-13 14:23:34 +0100

    creato header component

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <header class="container">
+      <h1>{{ title }}</h1>
+    </header>
+  </template>
+  
+  <script>
+  export default {
+    name: "HeaderComponent",
+    props: ["title"],
+    data() {
+      return {};
+    },
+  };
+  </script>
+  
+  <style lang="scss" scoped></style>
+  
