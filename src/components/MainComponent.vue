2d7aa01a445c99b0ad6879a03e849d3a2d8b3a7b
RickCraco <riccardocracolici43@gmail.com>
2023-11-13 14:26:19 +0100

    aggiunto main component

---
+++ src/components/MainComponent.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="row g-3 py-3">
+      <CardComponent
+        v-for="(el, index) in store.characterList"
+        :image="el.image"
+        :name="el.name"
+        :specie="el.species"
+        :state="el.status"
+      />
+    </div>
+  </template>
+  
+  <script>
+  import CardComponent from "./CardComponent.vue";
+  import { store } from "../data/store";
+  export default {
+    name: "MainComponent",
+    components: {
+      CardComponent,
+    },
+    data() {
+      return {
+        store,
+      };
+    },
+    methods: {},
+  };
+  </script>
+  
+  <style lang="scss" scoped></style>
+  
