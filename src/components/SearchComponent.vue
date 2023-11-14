7978b718de5cda5adbd71da8da921e69c26e3bc4
RickCraco <riccardocracolici43@gmail.com>
2023-11-14 14:12:57 +0100

    creato search component per trovare le carte per archetipo

---
+++ src/components/SearchComponent.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="bg-warning py-4">
+      <div class="container">
+        <select
+          class="form-select"
+          aria-label="Default select example"
+          v-model="filterValue"
+        >
+          <option value="">All cards</option>
+          <option
+            v-for="(el, index) in store.cardArchetypes"
+            :value="el.archetype_name"
+          >
+            {{ el.archetype_name }}
+          </option>
+        </select>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import { store } from "../data/store";
+  export default {
+    name: "SearchComponent",
+    data() {
+      return {
+        store,
+        filterValue: "",
+      };
+    },
+    methods() {},
+  };
+  </script>
+  
+  <style lang="scss" scoped>
+  .form-select {
+    width: 30% !important;
+  }
+  </style>
